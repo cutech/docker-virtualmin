eaf862faa3eb1dff8c753179d3906c43a385f1d7
Cody Unrath <cody@c-u-tech.com>
2017-11-20 12:22:07 -0800

    Delete cmd.sh

--- deb-stretch/cmd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-printf ${PASSWD}\\n${PASSWD}\\n | passwd \
-&& cat /etc/resolv.conf > resolv.conf \
-&& sed -i 's/# //'g resolv.conf \
-&& cat resolv.conf > /etc/resolv.conf \
-&& screen -dmS ops -c "/bin/bash"
